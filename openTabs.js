1593081317ca6d32367245c8ed6846f529a3ce2b
Neil Adit <neiladit@gmail.com>
2015-12-24 02:57:30 +0530

    Update openTabs.js

--- openTabs.js
+++ openTabs.js
@@ -1,28 +1,34 @@
 
-var fb = window.open('http://fb.com/', '_blank');
-if(fb){
-    fb.focus();
-}else{
-    alert('Please allow popups for this site');
+function openall(){
+    
+    var fb = window.open('http://fb.com/', '_blank');
+    if(fb){
+        fb.focus();
+    }else{
+        alert('Please allow popups for this site');
+    }
+    
+    var qr = window.open('http://quora.com/', '_blank');
+    if(qr){
+        qr.focus();
+    }else{
+        alert('Please allow popups for this site');
+    }
+    
+    var gmail = window.open('http://gmail.com/', '_blank');
+    if(gmail){
+        gmail.focus();
+    }else{
+        alert('Please allow popups for this site');
+    }
+    
+    var intern = window.open('http://placements.iitb.ac.in/trainingblog/', '_blank');
+    if(intern){
+        intern.focus();
+    }else{
+        alert('Please allow popups for this site');
+    }
+
 }
 
-var qr = window.open('http://quora.com/', '_blank');
-if(qr){
-    qr.focus();
-}else{
-    alert('Please allow popups for this site');
-}
-
-var gmail = window.open('http://gmail.com/', '_blank');
-if(gmail){
-    gmail.focus();
-}else{
-    alert('Please allow popups for this site');
-}
-
-var intern = window.open('http://placements.iitb.ac.in/trainingblog/', '_blank');
-if(intern){
-    intern.focus();
-}else{
-    alert('Please allow popups for this site');
-}
+chrome.browserAction.onClicked.addListener(openall);
