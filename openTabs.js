9a0e13640e3dec7c2c2f27c09796fc8a98d0e559
Neil Adit <neiladit@gmail.com>
2015-12-24 05:06:29 +0530

    Update openTabs.js

--- openTabs.js
+++ openTabs.js
@@ -1,28 +1,48 @@
 
-var fb1 = true;
-var qr1 = true;
-var gmail1 = true;
-var intern1  true;
+
+
+alert("here");
+console.log("Hello, world!");
 
 function openall(){
     
-    chrome.windows.getAll({"populate" : true},checkTabs);
+	var fb1 = true;
+	var qr1 = true;
+	var gmail1 = true;
+	var intern1 =true;
+    	
+	chrome.tabs.getAllInWindow(null, function(tabs){
+    for (var i = 0; i < tabs.length; i++) {
+		
+		console.log(tabs[i].url);
+		console.log(tabs[i].title);
+		
+	var prefix = /^https?:\/\//i;
+    var domain = /^[^\/]+/;
+   
+    tabs[i].url = tabs[i].url.replace(prefix, "");
     
-
-}
-
-function checkTabs(tab_array){
-        
-        for(var i=0;i<tab_array.length;i++)
-	{
-	    if(tab_array[i].url=='http://facebook.com/') fb1 = false;
-	    if(tab_array[i].url=='http://quora.com/') qr1 = false;
-	    if(tab_array[i].url=='http://gmail.com/') gmail1 = false;
-	    if(tab_array[i].url=='http://placements.iitb.ac.in/trainingblog/') intern1 = false;
-	    
+    if (tabs[i].url.charAt(0) === "/") {
+        tabs[i].url = window.location.hostname + tabs[i].url;
+    }
+    
+    var match = tabs[i].url.match(domain);
+    if (match) {
+        console.log(match[0]);
+    }
+	    if(match[0]=='www.facebook.com') fb1= false;
+		
+	    if(match[0]=='www.quora.com') qr1 = false;
+		
+	    if(match[0]=='mail.google.com') gmail1 = false;
+		
+	    if(match[0]=='placements.iitb.ac.in') intern1 = false;
+		
+		
+		
 	}
-        
-    if(fb1){
+    
+     if(fb1){
     
         var fb = window.open('http://fb.com/', '_blank');
         if(fb){
@@ -31,6 +51,7 @@ function checkTabs(tab_array){
             alert('Please allow popups for this site');
         }
     }
+	else alert("Facebook Already open");
     
     if(qr1){
     
@@ -41,8 +62,10 @@ function checkTabs(tab_array){
             alert('Please allow popups for this site');
         }
     }
+	else alert("Quora Already open");
     
     if(gmail1){
+		console.log("Hello");
         var gmail = window.open('http://gmail.com/', '_blank');
         if(gmail){
             gmail.focus();
@@ -50,6 +73,7 @@ function checkTabs(tab_array){
             alert('Please allow popups for this site');
         }
     }
+	else alert("Gmail Already open");
     
     if(intern1) {
         var intern = window.open('http://placements.iitb.ac.in/trainingblog/', '_blank');
@@ -58,7 +82,13 @@ function checkTabs(tab_array){
         }else{
             alert('Please allow popups for this site');
         }
-    }
+	}
+	else alert("Placement Blog Already open"); 
+
+});
+
 }
 
+
 chrome.browserAction.onClicked.addListener(openall);
+
